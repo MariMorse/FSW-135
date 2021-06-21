315615ccfde4fcc85b41ae0f769c30dbb44b4a8e
MariMorse <marianne.morse@bryanuniversity.edu>
2021-06-20 22:45:47 -0400

    commit

--- e-commerce/App.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose")
-
-app.listen(3000, () => {
-    console.log("The App is listening on PORT 3000.")
-});
