dd2148bc14028c695df59707f66874a5144fff4d
MariMorse <marianne.morse@bryanuniversity.edu>
2021-07-20 12:02:37 -0400

    commit

--- e-commerce/models/inventory.js
+++ e-commerce/models/inventory.js
@@ -2,5 +2,18 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema
 
 const inventorySchema = new mongoose.Schema({
-    
+
+    item: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    quantity: Number
 })
+
+module.exports = mongoose.model('Inventory', inventorySchema)
