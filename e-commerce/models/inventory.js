db4526cc272ccc461def09d2cc2d1ed22ed7f448
MariMorse <marianne.morse@bryanuniversity.edu>
2021-07-20 12:29:47 -0400

    commit

--- e-commerce/models/inventory.js
+++ e-commerce/models/inventory.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const inventorySchema = new mongoose.Schema({
+const inventorySchema = new Schema({
 
     item: {
         type: String,
